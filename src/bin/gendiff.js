e4386cec25d57ca85d19d62a04a712baf4fb670f
Miklashevsky Kirill <klmiklashevsky@gmail.com>
2020-01-13 22:55:09 +0300

    add handler to comparing two plain JSON files

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -2,13 +2,14 @@
 
 import commander from 'commander';
 import differ from '..';
+import { version } from '../../package.json';
 
 const program = new commander.Command();
 
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
-  .version('0.0.1')
+  .version(version)
   .option('-f, --format <type>', 'Output format')
   .arguments('<firstConfig> <secondConfig>')
   .action(differ)
--- src/index.js
+++ src/index.js
@@ -1,5 +1,34 @@
-export default (firstConfig, secondConfig) => {
-  const diff = 'difference';
+import fs from 'fs';
+import _ from 'lodash';
 
-  return diff;
+const getContent = (configPath) => {
+  const fileContent = fs.readFileSync(configPath);
+
+  return JSON.parse(fileContent);
+};
+
+export default (firstConfig, secondConfig) => {
+  const contentFirstConfig = getContent(firstConfig);
+  const contentSecondConfig = getContent(secondConfig);
+
+  const addedProps = Object.keys(contentSecondConfig)
+    .filter((item) => !_.has(contentFirstConfig, item))
+    .map((item) => `+ ${item}: ${contentSecondConfig[item]}`);
+
+  const deletedProps = Object.keys(contentFirstConfig)
+    .filter((item) => !_.has(contentSecondConfig, item));
+
+  const changedAndDeleted = Object.keys(contentFirstConfig).reduce((acc, item) => {
+    if (deletedProps.includes(item)) {
+      return [...acc, `- ${item}: ${contentFirstConfig[item]}`];
+    }
+    if (contentFirstConfig[item] === contentSecondConfig[item]) {
+      return [...acc, `  ${item}: ${contentFirstConfig[item]}`];
+    }
+    return [...acc, `+ ${item}: ${contentSecondConfig[item]}\n- ${item}: ${contentFirstConfig[item]}`];
+  }, []);
+
+  const difference = _.concat(['{'], changedAndDeleted, addedProps, ['}']).join('\n');
+
+  return difference;
 };
