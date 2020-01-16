88e8009f31f8c6817b53dffc97cded05d945167d
Miklashevsky Kirill <klmiklashevsky@gmail.com>
2020-01-16 21:13:35 +0300

    add additional tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,10 +1,23 @@
 import fs from 'fs';
 import gendiff from '../src';
 
+const firstConfig = `${__dirname}/__fixtures__/before.json`;
+const secondConfig = `${__dirname}/__fixtures__/after.json`;
+const emptyConfig = `${__dirname}/__fixtures__/empty.json`;
+
 test('compare two JSON configs', () => {
   const result1 = fs.readFileSync(`${__dirname}/__fixtures__/result1.txt`, 'utf8');
-  const firstConfig = `${__dirname}/__fixtures__/before.json`;
-  const secondConfig = `${__dirname}/__fixtures__/after.json`;
+  const result2 = fs.readFileSync(`${__dirname}/__fixtures__/result2.txt`, 'utf8');
 
   expect(gendiff(firstConfig, secondConfig)).toEqual(result1);
+  expect(gendiff(emptyConfig, emptyConfig)).toEqual('{\n}');
+  expect(gendiff(firstConfig, firstConfig)).toEqual(result2);
+});
+
+test('compare with empty config', () => {
+  const result3 = fs.readFileSync(`${__dirname}/__fixtures__/result3.txt`, 'utf8');
+  const result4 = fs.readFileSync(`${__dirname}/__fixtures__/result4.txt`, 'utf8');
+
+  expect(gendiff(firstConfig, emptyConfig)).toEqual(result3);
+  expect(gendiff(emptyConfig, firstConfig)).toEqual(result4);
 });
