c26827d5f3140d8c42000ccabdb9e22c1cf2d5dc
Miklashevsky Kirill <klmiklashevsky@gmail.com>
2020-01-21 20:47:23 +0300

    add tests for YML configs

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -3,11 +3,11 @@ import gendiff from '../src';
 
 const pathToFixtures = `${__dirname}/../__fixtures__`;
 
-const firstConfig = `${pathToFixtures}/before.json`;
-const secondConfig = `${pathToFixtures}/after.json`;
-const emptyConfig = `${pathToFixtures}/empty.json`;
-
 test('compare two JSON configs', () => {
+  const firstConfig = `${pathToFixtures}/before.json`;
+  const secondConfig = `${pathToFixtures}/after.json`;
+  const emptyConfig = `${pathToFixtures}/empty.json`;
+
   const result1 = fs.readFileSync(`${pathToFixtures}/result1.txt`, 'utf8');
   const result2 = fs.readFileSync(`${pathToFixtures}/result2.txt`, 'utf8');
 
@@ -20,4 +20,14 @@ test('compare two JSON configs', () => {
 
   expect(gendiff(firstConfig, emptyConfig)).toEqual(result3);
   expect(gendiff(emptyConfig, firstConfig)).toEqual(result4);
+});
+
+test('compare two YML configs', () => {
+  const firstConfigYML = `${pathToFixtures}/before.yml`;
+  const secondConfigYML = `${pathToFixtures}/after.yml`;
+
+  const resultYML1 = fs.readFileSync(`${pathToFixtures}/resultYML1.txt`, 'utf8');
+  // const resultYML2 = fs.readFileSync(`${pathToFixtures}/resultYML2.txt`, 'utf8');
+
+  expect(gendiff(firstConfigYML, secondConfigYML)).toEqual(resultYML1);
 });
