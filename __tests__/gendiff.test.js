2ffe83adffed835f9ef59590f49cd1df4dd0852f
Miklashevsky Kirill <klmiklashevsky@gmail.com>
2020-01-25 21:41:18 +0300

    add tests for .ini configs

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -3,7 +3,7 @@ import gendiff from '../src';
 
 const pathToFixtures = `${__dirname}/../__fixtures__`;
 
-test('compare two JSON configs', () => {
+test('compare two .json configs', () => {
   const firstConfig = `${pathToFixtures}/before.json`;
   const secondConfig = `${pathToFixtures}/after.json`;
   const emptyConfig = `${pathToFixtures}/empty.json`;
@@ -22,12 +22,20 @@ test('compare two JSON configs', () => {
   expect(gendiff(emptyConfig, firstConfig)).toEqual(result4);
 });
 
-test('compare two YML configs', () => {
+test('compare two .yml configs', () => {
   const firstConfigYML = `${pathToFixtures}/before.yml`;
   const secondConfigYML = `${pathToFixtures}/after.yml`;
 
   const resultYML1 = fs.readFileSync(`${pathToFixtures}/resultYML1.txt`, 'utf8');
-  // const resultYML2 = fs.readFileSync(`${pathToFixtures}/resultYML2.txt`, 'utf8');
 
   expect(gendiff(firstConfigYML, secondConfigYML)).toEqual(resultYML1);
+});
+
+test('compare two .ini configs', () => {
+  const firstConfigINI = `${pathToFixtures}/before.ini`;
+  const secondConfigINI = `${pathToFixtures}/after.ini`;
+
+  const resultINI1 = fs.readFileSync(`${pathToFixtures}/resultINI1.txt`, 'utf8');
+
+  expect(gendiff(firstConfigINI, secondConfigINI)).toEqual(resultINI1);
 });
