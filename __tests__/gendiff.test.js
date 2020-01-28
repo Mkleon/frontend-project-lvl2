9beea0f94474758581b6fbb6799cf172425c671b
Miklashevsky Kirill <klmiklashevsky@gmail.com>
2020-01-28 21:05:38 +0300

    refactoring tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -2,40 +2,13 @@ import fs from 'fs';
 import gendiff from '../src';
 
 const pathToFixtures = `${__dirname}/../__fixtures__`;
+const exts = ['json', 'yml', 'ini'];
 
-test('compare two .json configs', () => {
-  const firstConfig = `${pathToFixtures}/before.json`;
-  const secondConfig = `${pathToFixtures}/after.json`;
-  const emptyConfig = `${pathToFixtures}/empty.json`;
+const result = fs.readFileSync(`${pathToFixtures}/result.txt`, 'utf8');
 
-  const result1 = fs.readFileSync(`${pathToFixtures}/result1.txt`, 'utf8');
-  const result2 = fs.readFileSync(`${pathToFixtures}/result2.txt`, 'utf8');
+test.each(exts)('compare two .json configs', (ext) => {
+  const firstConfig = `${pathToFixtures}/before.${ext}`;
+  const secondConfig = `${pathToFixtures}/after.${ext}`;
 
-  expect(gendiff(firstConfig, secondConfig)).toEqual(result1);
-  expect(gendiff(emptyConfig, emptyConfig)).toEqual('{\n}');
-  expect(gendiff(firstConfig, firstConfig)).toEqual(result2);
-
-  const result3 = fs.readFileSync(`${pathToFixtures}/result3.txt`, 'utf8');
-  const result4 = fs.readFileSync(`${pathToFixtures}/result4.txt`, 'utf8');
-
-  expect(gendiff(firstConfig, emptyConfig)).toEqual(result3);
-  expect(gendiff(emptyConfig, firstConfig)).toEqual(result4);
-});
-
-test('compare two .yml configs', () => {
-  const firstConfigYML = `${pathToFixtures}/before.yml`;
-  const secondConfigYML = `${pathToFixtures}/after.yml`;
-
-  const resultYML1 = fs.readFileSync(`${pathToFixtures}/resultYML1.txt`, 'utf8');
-
-  expect(gendiff(firstConfigYML, secondConfigYML)).toEqual(resultYML1);
-});
-
-test('compare two .ini configs', () => {
-  const firstConfigINI = `${pathToFixtures}/before.ini`;
-  const secondConfigINI = `${pathToFixtures}/after.ini`;
-
-  const resultINI1 = fs.readFileSync(`${pathToFixtures}/resultINI1.txt`, 'utf8');
-
-  expect(gendiff(firstConfigINI, secondConfigINI)).toEqual(resultINI1);
+  expect(gendiff(firstConfig, secondConfig)).toEqual(result);
 });
