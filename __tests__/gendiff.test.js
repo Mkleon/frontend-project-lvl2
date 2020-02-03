34cf74198904457284facebda60046176ef02781
Miklashevsky Kirill <klmiklashevsky@gmail.com>
2020-02-03 22:00:33 +0300

    change fixtures

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -4,13 +4,15 @@ import gendiff from '../src';
 const pathToFixtures = `${__dirname}/../__fixtures__`;
 const exts = ['json', 'yml', 'ini'];
 
-const resultJson = fs.readFileSync(`${pathToFixtures}/result.txt`, 'utf8');
+const resultTree = fs.readFileSync(`${pathToFixtures}/result.txt`, 'utf8');
 const resultPlain = fs.readFileSync(`${pathToFixtures}/result plain.txt`, 'utf8');
+const resultJSON = fs.readFileSync(`${pathToFixtures}/result json.txt`, 'utf8');
 
 test.each(exts)('compare two configs', (ext) => {
   const firstConfig = `${pathToFixtures}/before.${ext}`;
   const secondConfig = `${pathToFixtures}/after.${ext}`;
 
-  expect(gendiff(firstConfig, secondConfig, 'json')).toEqual(resultJson);
+  expect(gendiff(firstConfig, secondConfig, 'tree')).toEqual(resultTree);
   expect(gendiff(firstConfig, secondConfig, 'plain')).toEqual(resultPlain);
+  expect(gendiff(firstConfig, secondConfig, 'json')).toEqual(resultJSON);
 });
