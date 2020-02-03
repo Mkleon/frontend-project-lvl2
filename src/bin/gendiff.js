b16062452517a84c4d59b56655870ce54cb0e531
Miklashevsky Kirill <klmiklashevsky@gmail.com>
2020-02-03 21:30:18 +0300

    refactor execute file

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -5,20 +5,15 @@ import gendiff from '..';
 import { version } from '../../package.json';
 
 const program = new commander.Command();
-let first;
-let second;
 
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version(version)
-  .option('-f, --format [type]', 'Output format', 'json')
+  .option('-f, --format [type]', 'output format (json|tree|plain)', 'json')
   .arguments('<firstConfig> <secondConfig>')
   .action((firstConfig, secondConfig) => {
-    first = firstConfig;
-    second = secondConfig;
+    console.log(gendiff(firstConfig, secondConfig, program.format));
   });
 
 program.parse(process.argv);
-
-console.log(gendiff(first, second, program.format));
