ffa2b0a4bc5468d5ee6aecaea20249200828123b
Miklashevsky Kirill <klmiklashevsky@gmail.com>
2020-02-17 20:48:52 +0300

    refactor tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -4,13 +4,13 @@ import gendiff from '../src';
 const pathToFixtures = `${__dirname}/../__fixtures__`;
 const exts = ['json', 'yml', 'ini'];
 
-const resultTree = fs.readFileSync(`${pathToFixtures}/result.txt`, 'utf8');
-const resultPlain = fs.readFileSync(`${pathToFixtures}/result plain.txt`, 'utf8');
-const resultJSON = fs.readFileSync(`${pathToFixtures}/result json.json`, 'utf8');
-
 describe.each(exts)('compare two configs', (ext) => {
   const firstConfig = `${pathToFixtures}/before.${ext}`;
   const secondConfig = `${pathToFixtures}/after.${ext}`;
+
+  const resultTree = fs.readFileSync(`${pathToFixtures}/result_tree.txt`, 'utf8');
+  const resultPlain = fs.readFileSync(`${pathToFixtures}/result_plain.txt`, 'utf8');
+  const resultJSON = fs.readFileSync(`${pathToFixtures}/result_json.json`, 'utf8');
 
   test(`compare ${ext} files`, () => {
     expect(gendiff(firstConfig, secondConfig, 'tree')).toEqual(resultTree);
