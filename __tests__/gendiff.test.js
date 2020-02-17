2ea7de24fd3048bb11a0d303a364a90846a32b37
Miklashevsky Kirill <klmiklashevsky@gmail.com>
2020-02-17 21:12:49 +0300

    add results into beforeEach

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -4,13 +4,19 @@ import gendiff from '../src';
 const pathToFixtures = `${__dirname}/../__fixtures__`;
 const exts = ['json', 'yml', 'ini'];
 
+let resultTree;
+let resultPlain;
+let resultJSON;
+
+beforeEach(() => {
+  resultTree = fs.readFileSync(`${pathToFixtures}/result_tree.txt`, 'utf8');
+  resultPlain = fs.readFileSync(`${pathToFixtures}/result_plain.txt`, 'utf8');
+  resultJSON = fs.readFileSync(`${pathToFixtures}/result_json.txt`, 'utf8');
+});
+
 describe.each(exts)('compare two configs', (ext) => {
   const firstConfig = `${pathToFixtures}/before.${ext}`;
   const secondConfig = `${pathToFixtures}/after.${ext}`;
-
-  const resultTree = fs.readFileSync(`${pathToFixtures}/result_tree.txt`, 'utf8');
-  const resultPlain = fs.readFileSync(`${pathToFixtures}/result_plain.txt`, 'utf8');
-  const resultJSON = fs.readFileSync(`${pathToFixtures}/result_json.txt`, 'utf8');
 
   test(`compare ${ext} files`, () => {
     expect(gendiff(firstConfig, secondConfig, 'tree')).toEqual(resultTree);
