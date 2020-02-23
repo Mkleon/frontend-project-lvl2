344dc19d14675576dd72995967cc3e5503ba6f92
Miklashevsky Kirill <klmiklashevsky@gmail.com>
2020-02-23 15:18:01 +0300

    refactor tree formatter

--- src/formatters/tree.js
+++ src/formatters/tree.js
@@ -13,14 +13,14 @@ const createSpacesBeforeBracket = (level) => (
 const stringify = (value, spaces) => {
   const formats = [
     {
-      check: (obj) => obj instanceof Object,
+      check: (obj) => _.isObject(obj),
       format: (obj) => {
         const arr = Object.keys(obj).reduce((acc, item) => [...acc, `${spaces}      ${item}: ${obj[item]}`], []);
         return ['{', arr, `${spaces}  }`].join('\n');
       },
     },
     {
-      check: (obj) => !(obj instanceof Object),
+      check: (obj) => !_.isObject(obj),
       format: (obj) => _.identity(obj),
     },
   ];
@@ -33,20 +33,15 @@ const decorators = {
   deleted: ({ name, value }, level) => `${createSpaces(level)}- ${name}: ${stringify(value.valueBefore, createSpaces(level))}`,
   changed: ({ name, value }, level) => `${createSpaces(level)}+ ${name}: ${stringify(value.valueAfter, createSpaces(level))}\n${createSpaces(level)}- ${name}: ${stringify(value.valueBefore, createSpaces(level))}`,
   unchanged: ({ name, value }, level) => `${createSpaces(level)}  ${name}: ${stringify(value.valueBefore, createSpaces(level))}`,
-  nested: ({ name, value }, level, fn) => `${createSpaces(level)}  ${name}: ${fn([], value, level + 1)}`,
+  nested: ({ name, value }, level, fn) => `${createSpaces(level)}  ${name}: ${fn(value, level + 1)}`,
 };
 
 export default (tree) => {
-  const iter = (acc, node, level = 1) => {
-    const elem = node.reduce((innerAcc, item) => {
-      const { state } = item;
-      const newItem = decorators[state](item, level, iter);
+  const iter = (node, level = 1) => {
+    const newItem = node.map((item) => decorators[item.state](item, level, iter));
 
-      return [...innerAcc, newItem];
-    }, acc);
-
-    return ['{', ...elem, `${createSpacesBeforeBracket(level)}}`].join('\n');
+    return ['{', ...newItem, `${createSpacesBeforeBracket(level)}}`].join('\n');
   };
 
-  return [iter([], tree)].join('\n');
+  return [iter(tree)].join('\n');
 };
